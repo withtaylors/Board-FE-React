6ba59735eb89230ac1f9ab1ddd6be9c18887d1c1
withtaylors <withtaylors@naver.com>
2023-12-02 16:44:51 +0900

    --

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route, useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/home/Home";
 import SignUp from "./pages/sign-up/SignUp";
@@ -11,41 +11,41 @@ import MyBoardList from "./pages/myboard-list/MyBoardList";
 import Board from "./pages/board/Board";
 import EditBoard from "./pages/edit-board/EditBoard";
 
-
 const App = () => {
   const location = useLocation();
 
   return (
     <React.Fragment>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route
           path="/add-board"
-          element={
-            <PrivateRoute path="/add-board" component={AddBoard}/>
-          }
+          element={<PrivateRoute path="/add-board" component={AddBoard} />}
         />
-        <Route path="/board-list" element={<BoardList/>}/>
+        <Route path="/board-list" element={<BoardList />} />
         <Route
           path="/myboard-list"
           element={
             // 쿼리 파라미터가 존재하므로 전체 url을 PrivateRoute에 넘겨준다
-            <PrivateRoute path={`${location.pathname}`} component={MyBoardList}/>
+            <PrivateRoute
+              path={`${location.pathname}`}
+              component={MyBoardList}
+            />
           }
         />
-        <Route path="/board/:board_id" element={<Board/>}/>
+        <Route path="/board/:board_id" element={<Board />} />
         <Route
           path="/edit-board/:board_id"
           element={
             // URI 파라미터가 존재하므로 전체 url을 PrivateRoute에 넘겨준다
-            <PrivateRoute path={`${location.pathname}`} component={EditBoard}/>
+            <PrivateRoute path={`${location.pathname}`} component={EditBoard} />
           }
         />
       </Routes>
     </React.Fragment>
-  )
-}
+  );
+};
 export default App;
